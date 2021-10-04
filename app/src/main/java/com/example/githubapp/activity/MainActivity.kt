456af9ed63068ff50e0828bb7d38be732200c04d
Asep Sutisna <sutisnaasep323@gmail.com>
2021-10-04 22:49:13 +0700

    Menu Search

--- app/src/main/java/com/example/githubapp/activity/MainActivity.kt
+++ app/src/main/java/com/example/githubapp/activity/MainActivity.kt
@@ -1,30 +1,62 @@
 package com.example.githubapp.activity
 
+import android.app.SearchManager
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
+import androidx.appcompat.widget.SearchView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.githubapp.data.DummyData
 import com.example.githubapp.R
 import com.example.githubapp.model.User
 import com.example.githubapp.adapter.UserAdapter
+import com.example.githubapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     private lateinit var rvUser: RecyclerView
     private val list: ArrayList<User> = arrayListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         supportActionBar?.title = "User's"
 
-        rvUser = findViewById(R.id.rv_user)
+        rvUser = findViewById(R.id.recyclerView)
         rvUser.setHasFixedSize(true)
 
         list.addAll(DummyData.listData)
         showRecyclerList()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.option_menu, menu)
+
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        val searchView = menu.findItem(R.id.search).actionView as SearchView
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        searchView.queryHint = resources.getString(R.string.search_here)
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                //showLoading(true)
+                Toast.makeText(this@MainActivity, "Search",Toast.LENGTH_LONG).show()
+                return true
+            }
+            override fun onQueryTextChange(newText: String): Boolean {
+                return false
+            }
+        })
+        return super.onCreateOptionsMenu(menu)
     }
 
     private fun showRecyclerList() {
@@ -43,6 +75,14 @@ class MainActivity : AppCompatActivity() {
         val intent = Intent(this@MainActivity, DetailUserActivity::class.java)
         intent.putExtra(DetailUserActivity.USER, user)
         startActivity(intent)
+    }
+
+    private fun showLoading(state: Boolean) {
+        if (state) {
+            binding.progressBar.visibility = View.VISIBLE
+        } else {
+            binding.progressBar.visibility = View.GONE
+        }
     }
 
 
---
+++ app/src/main/java/com/example/githubapp/viewModel/MainViewModel.kt
@@ -0,0 +1,6 @@
+package com.example.githubapp.viewModel
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel: ViewModel()  {
+}
